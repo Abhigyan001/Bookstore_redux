b2d5a7479d6ee04bc952d3308e16e132775d77c6
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-25 13:53:21 +0530

    Implement the CategoryFilter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const categories = [
+  'All',
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
+const CategoryFilter = ({
+  changeFilter,
+}) => {
+  const selectCategory = React.useRef(null);
+  return (
+    <div>
+      <select ref={selectCategory} name="category" onChange={() => changeFilter(selectCategory.current.value)}>
+        {categories.map(category => (
+          <option key={category}>{category}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+CategoryFilter.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+};
+
+export default CategoryFilter;
